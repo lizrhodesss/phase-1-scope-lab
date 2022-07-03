865f6f9c2b5c3fbaa146c49b7864e804f96baa52
liz <rhodes.liz@gmail.com>
2022-07-03 13:23:13 -0400

    commit tree 93eed862e6312f1a760e772a9cf2461dd8455da2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'liz'
+
+
+function upperCaseCustomerName() {
+ customerName = customerName.toUpperCase()
+}
+
+// function  toUpperCase(customerName) {
+//     return customerName
+// } 
+function setBestCustomer() {
+    bestCustomer = 'not bob' 
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = 'Vic'
+}
